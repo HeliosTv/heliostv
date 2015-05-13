2b11cc8652cbf83de89b30cfe2222f6d2ae148df
Romain Picard <romain.picard@softathome.com>
2015-05-13 16:00:49 +0200

    Added libheliostv

---
+++ include/heliostv/ControlChannel.h
@@ -0,0 +1,36 @@
+#ifndef __CONTROLCHANNEL_H
+#define __CONTROLCHANNEL_H
+
+/*
+*****************************************************************************
+*                 Copyright (c) 2015 SoftAtHome
+*
+*     The computer program contained herein contains proprietary
+*     information which is the property of SoftAtHome.  The program
+*     may be used and/or copied only with the written permission of
+*     SoftAtHome or in accordance with the terms and conditions
+*     stipulated in the agreement/contract under which the programs
+*     have been supplied.
+*****************************************************************************/
+/**
+* @file     ControlChannel.h
+* @author   R. Picard
+* @date     13/05/2015
+*
+******************************************************************************
+*/
+
+namespace HeliosTv {
+
+class ControlChannel
+{
+    public:
+                ControlChannel(void);
+        virtual ~ControlChannel(void);
+};
+
+} // namespace
+
+
+#endif /* __CONTROLCHANNEL_H */
+
---
+++ libheliostv/ControlChannel.cpp
@@ -0,0 +1,31 @@
+/*
+*****************************************************************************
+*                 Copyright (c) 2015 SoftAtHome
+*
+*     The computer program contained herein contains proprietary
+*     information which is the property of SoftAtHome.  The program
+*     may be used and/or copied only with the written permission of
+*     SoftAtHome or in accordance with the terms and conditions
+*     stipulated in the agreement/contract under which the programs
+*     have been supplied.
+*****************************************************************************/
+/**
+* @file     ControlChannel.cpp
+* @author   R. Picard
+* @date     13/05/2015
+*
+******************************************************************************
+*/
+#include "heliostv/ControlChannel.h"
+
+namespace HeliosTv {
+
+ControlChannel::ControlChannel(void)
+{
+}
+
+ControlChannel::~ControlChannel(void)
+{
+}
+
+} // namespace
